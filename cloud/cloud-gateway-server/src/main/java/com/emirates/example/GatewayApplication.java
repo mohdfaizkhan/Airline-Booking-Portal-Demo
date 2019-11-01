49cf2044b43ec7e855c8a1d2a80b5b137ddff4b3
Mohdfaizkhan <mohdfaizcs059@gmail.com>
2019-11-01 20:25:46 +0400

    Changes

--- cloud/cloud-gateway-server/src/main/java/com/emirates/example/GatewayApplication.java
+++ cloud/cloud-gateway-server/src/main/java/com/emirates/example/GatewayApplication.java
@@ -1,17 +1,5 @@
 package com.emirates.example;
 
-/*-
- * #%L
- * configuration-server
- * %%
- * Copyright (C) 2018 Emirates Group IT
- * %%
- * This file is part of OCSL.
- *
- * OCSL can not be copied and/or distributed without the express permission of Emirates Group IT
- * #L%
- */
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
